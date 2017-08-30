1a8d43e634f47f01e4afbe2c1c0a3c512255ab23
Piti <piti118@gmail.com>
2017-08-30 15:12:00 +0700

    update

--- rps/src/App.js
+++ rps/src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import ListHand from './ListHand'
 import Login from './Login'
+import { withRouter } from 'react-router'
 
 function Register() {
   return (<div> Register Page</div>)
@@ -20,35 +21,40 @@ function Logout() {
 
 class App extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {username: null}
+  }
+
   onLoginSuccess(username) {
-    console.log('From App: Login Success', username)
+    this.setState({username})
+    this.props.history.push('/app/list-hand')
   }
 
   render() {
     return (
       <div>
-        <Router>
-          <div>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/app/register">Register</Link></li>
-              <li><Link to="/app/login">Login</Link></li>
-              <li><Link to="/app/list-hand">List Hand</Link></li>
-            </ul>
-            <Route exact path="/" component={Login}/>
-            <Route path="/app/register" component={Register}/>
-            <Route path="/app/login" render={()=>{
-              return <Login onLoginSuccess={this.onLoginSuccess.bind(this)}/>
-            }}/>
-            <Route path="/app/list-hand" component={ListHand}/>
-          </div>
-        </Router>
+        <div>
+          {this.state.username}
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/app/register">Register</Link></li>
+            <li><Link to="/app/login">Login</Link></li>
+            <li><Link to="/app/list-hand">List Hand</Link></li>
+          </ul>
+          <Route exact path="/" component={Login}/>
+          <Route path="/app/register" component={Register}/>
+          <Route path="/app/login" render={()=>{
+            return <Login onLoginSuccess={this.onLoginSuccess.bind(this)}/>
+          }}/>
+          <Route path="/app/list-hand" component={ListHand}/>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
 /*
 
 
